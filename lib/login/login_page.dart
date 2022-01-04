387df7ed8a065b32593094f50ce67b68b978db4d
ebrar <92300256+ebrarakcaseven@users.noreply.github.com>
2022-01-04 16:42:06 +0300

    bottum

--- lib/login/login_page.dart
+++ lib/login/login_page.dart
@@ -1,3 +1,4 @@
+import 'package:app_design/login/register_page.dart';
 import 'package:flutter/material.dart';
 
 class LoginPage extends StatefulWidget {
@@ -12,115 +13,146 @@ class _LoginPageState extends State<LoginPage> {
   Widget build(BuildContext context) {
     var size = MediaQuery.of(context).size;
     return Scaffold(
-        body: Stack(
-      children: [
+      body: Stack(children: [
         Center(
-          child: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 10.0),
-            child: Container(
-              height: size.height * .4,
-              width: size.width * .85,
-              decoration: BoxDecoration(
-                  color: const Color(0xFFECDDFF),
-                  borderRadius: const BorderRadius.all(Radius.circular(20)),
-                  boxShadow: [
-                    BoxShadow(
-                        color: Colors.grey.withOpacity(.75),
-                        blurRadius: 10,
-                        spreadRadius: 2)
-                  ]),
-              child: Padding(
-                padding: const EdgeInsets.all(10.0),
+          child: Column(
+            children: [
+              Container(
+                  height: 380.0,
+                  width: 400.0,
+                  child: Image.asset('images/img3.jpg')),
+              Container(
+                height: size.height * .4,
+                width: size.width * .85,
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                ),
                 child: Center(
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      const TextField(
-                          style: TextStyle(
-                            color: Colors.black,
-                          ),
-                          cursorColor: Colors.black,
-                          keyboardType: TextInputType.emailAddress,
-                          decoration: InputDecoration(
-                            prefixIcon: Icon(
-                              Icons.mail,
-                              color: Colors.black,
-                            ),
-                            hintText: 'E-Mail',
-                            prefixText: ' ',
-                            hintStyle: TextStyle(color: Colors.black),
-                            focusColor: Colors.black,
-                            focusedBorder: UnderlineInputBorder(
-                                borderSide: BorderSide(
-                              color: Colors.black,
-                            )),
-                            enabledBorder: UnderlineInputBorder(
-                                borderSide: BorderSide(
-                              color: Colors.black,
-                            )),
-                          )),
-                      SizedBox(
-                        height: size.height * 0.02,
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 10.0),
+                    child: Container(
+                      height: size.height * .4,
+                      width: size.width * .85,
+                      decoration: BoxDecoration(
+                        color: Colors.white,
                       ),
-                      const TextField(
-                          style: TextStyle(
-                            color: Colors.black,
-                          ),
-                          cursorColor: Colors.black,
-                          obscureText: true,
-                          decoration: InputDecoration(
-                            prefixIcon: Icon(
-                              Icons.vpn_key,
-                              color: Colors.black,
-                            ),
-                            hintText: 'Parola',
-                            prefixText: ' ',
-                            hintStyle: TextStyle(color: Colors.black),
-                            focusColor: Colors.black,
-                            focusedBorder: UnderlineInputBorder(
-                                borderSide: BorderSide(
-                              color: Colors.black,
-                            )),
-                            enabledBorder: UnderlineInputBorder(
-                                borderSide: BorderSide(
-                              color: Colors.black,
-                            )),
-                          )),
-                      SizedBox(
-                        height: size.height * 0.08,
-                      ),
-                      InkWell(
-                        onTap: () {},
-                        child: Container(
-                          padding: const EdgeInsets.symmetric(vertical: 5),
-                          decoration: BoxDecoration(
-                              border: Border.all(color: Colors.black, width: 2),
-                              //color: colorPrimaryShade,
-                              borderRadius:
-                                  const BorderRadius.all(Radius.circular(30))),
-                          child: const Padding(
-                            padding: EdgeInsets.all(5.0),
-                            child: Center(
-                                child: Text(
-                              "Login",
-                              style: TextStyle(
-                                color: Colors.black,
-                                fontSize: 20,
+                      child: Padding(
+                        padding: const EdgeInsets.all(10.0),
+                        child: Center(
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              const TextField(
+                                  style: TextStyle(
+                                    color: Colors.black,
+                                  ),
+                                  cursorColor: Colors.black,
+                                  keyboardType: TextInputType.emailAddress,
+                                  decoration: InputDecoration(
+                                    border: OutlineInputBorder(),
+                                    prefixIcon: Icon(
+                                      Icons.mail,
+                                      color: Colors.black,
+                                    ),
+                                    hintText: 'E-Mail',
+                                    prefixText: ' ',
+                                    hintStyle: TextStyle(color: Colors.black),
+                                    focusColor: Colors.black,
+                                  )),
+                              SizedBox(
+                                height: size.height * 0.01,
                               ),
-                            )),
+                              const TextField(
+                                  style: TextStyle(
+                                    color: Colors.black,
+                                  ),
+                                  cursorColor: Colors.black,
+                                  obscureText: true,
+                                  decoration: InputDecoration(
+                                    border: OutlineInputBorder(),
+                                    prefixIcon: Icon(
+                                      Icons.vpn_key,
+                                      color: Colors.black,
+                                    ),
+                                    hintText: 'Password',
+                                    prefixText: ' ',
+                                    hintStyle: TextStyle(color: Colors.black),
+                                    focusColor: Colors.black,
+                                  )),
+                              SizedBox(
+                                height: size.height * 0.01,
+                              ),
+                              InkWell(
+                                onTap: () {},
+                                child: Container(
+                                  padding:
+                                      const EdgeInsets.symmetric(vertical: 5),
+                                  decoration: BoxDecoration(
+                                      border: Border.all(
+                                          color: Colors.black, width: 2),
+                                      borderRadius: const BorderRadius.all(
+                                          Radius.circular(6))),
+                                  child: const Padding(
+                                    padding: EdgeInsets.all(5.0),
+                                    child: Center(
+                                        child: Text(
+                                      "Login",
+                                      style: TextStyle(
+                                        color: Colors.black,
+                                        fontSize: 20,
+                                      ),
+                                    )),
+                                  ),
+                                ),
+                              ),
+                              SizedBox(
+                                height: size.height * 0.02,
+                              ),
+                              InkWell(
+                                onTap: () {
+                                  Navigator.push(
+                                      context,
+                                      MaterialPageRoute(
+                                          builder: (context) => Register()));
+                                },
+                                child: Row(
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceEvenly,
+                                  // ignore: prefer_const_literals_to_create_immutables
+                                  children: [
+                                    const Text(
+                                      "Do you have an account?",
+                                      style: TextStyle(color: Colors.black),
+                                    ),
+                                    InkWell(
+                                      child: Text("Create account",
+                                          style: TextStyle(
+                                            fontWeight: FontWeight.bold,
+                                          )),
+                                      onTap: () => Navigator.push(
+                                        context,
+                                        MaterialPageRoute(
+                                            builder: (context) =>
+                                                const Register()),
+                                      ),
+                                    )
+                                  ],
+                                ),
+                              )
+                            ],
                           ),
                         ),
                       ),
-                    ],
+                    ),
                   ),
                 ),
               ),
-            ),
+            ],
           ),
         ),
         Padding(
-          padding:
-              EdgeInsets.only(top: size.height * .06, left: size.width * .02),
+          padding: EdgeInsets.only(
+              top: size.height * .06, left: size.width * .02, bottom: 10.0),
           child: Align(
             alignment: Alignment.topLeft,
             child: Row(
@@ -133,21 +165,11 @@ class _LoginPageState extends State<LoginPage> {
                     size: 26,
                   ),
                 ),
-                SizedBox(
-                  width: size.width * 0.3,
-                ),
-                const Text(
-                  "Login",
-                  style: TextStyle(
-                      fontSize: 20,
-                      color: Colors.black,
-                      fontWeight: FontWeight.bold),
-                )
               ],
             ),
           ),
         )
-      ],
-    ));
+      ]),
+    );
   }
 }
